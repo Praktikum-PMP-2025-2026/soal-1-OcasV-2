521166822229dbfcddfb4de377f65f2152ec4597
OcasV <farrasfuadyh@gmail.com>
2026-04-22 09:51:38 +0700

    #2
    Membenarkan  fungsi pada main

--- soal1.c
+++ soal1.c
@@ -1,44 +1,66 @@
+/*
+* Modul     : 2 - Data Manipulation and External File
+* Pembuat   : Farras Fuady Hakim (13224047)
+* Deskripsi : Program untuk membaca string dan menentukan sum, average, max, dan index dari max.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void operasi(char *arr, int N, int *sum, float *avg, int *max, int *id);
-
-void operasi(char *arr, int N, int *sum, float *avg, int *max, int *id) {
+void operasi(char *arr, int *sum, float *avg, int *max, int *idx) {
+    float fsum = 0;
+    int fmax = 0;
+    int fidx = 0;
     int next = 0;
-    
-    max = 0;
-    avg = 0;
-    id = 0;
+    float favg = 0;
 
-    char *trash = strtok(arr, " ");
+    printf("%s\n", arr);
 
-    for (int i = 0; i < N; i++) {
-        next = atoi(strtok(arr, " "));
-        if (next > *max) {
-            *max = next;
-            *id = i + 1;
+    float N = atoi(strtok(arr, " "));
+
+    printf("%s\n", arr);
+
+    for (int i = 0; i < (int)N; i++) {
+        next = atoi(strtok(NULL, " "));
+        printf("%d\n", next);
+        if (next > fmax) {
+            fmax = next;
+            fidx = i;
         }
-        *sum += next;
+        fsum += next;
     }
 
-    *avg = *sum/N;
+    printf("1\n");
+
+    favg = fsum/N;
+
+    *sum = fsum;
+    *avg = favg;
+    printf("2\n");
+    *max = fmax;
+    *idx = fidx;
+    printf("3\n");
 }
 
 int main() {
-    char *input;
-    int *sum, *max, *id;
+    char input;
+    int sum, max, id;
     int N;
-    float *avg;
+    float avg;
 
-    scanf("%s", &input);
+    strcpy(&input, "5 7 2 9 9 4");
 
-    N =  atoi(strtok(input, " "));
+    printf("%s\n", &input);
 
-    operasi(input, N, sum, avg, max, id);
+    // scanf("%s", &input);
+
+    operasi(&input, &sum, &avg, &max, &id);
 
     printf("SUM %d\n", sum);
     printf("AVG %.2f\n", avg);
     printf("MAX %d\n", max);
     printf("IDX %d\n", id);
+
+    return 0;
 }
